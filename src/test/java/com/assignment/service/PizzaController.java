1310bcf7ee19f6dd20263f9b835ed80f8fe0e44a
Sid Dutta <siddhartha.dutta@cerner.com>
2018-10-02 14:39:13 -0500

    controller

--- src/test/java/com/assignment/service/PizzaController.java
+++ src/test/java/com/assignment/service/PizzaController.java
@@ -2,12 +2,13 @@ package com.assignment.service;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PizzaController {
 
-    @RequestMapping("/root")
+    @RequestMapping(value = "/root", method = RequestMethod.GET)
     public @ResponseBody String greeting() {
         return "Root context for the pizza service";
     }
